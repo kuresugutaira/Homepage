616f7e63e84c4478c91664a9ac45458f0a20c0c0
kuresugutaira <kuresugutaira@gmail.com>
2022-12-10 02:11:11 +0900

    noticeコンポーネントの適用、一部細かいところ修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
 import Link from 'next/link';
 import Icon from '../components/utils/icon/icon';
+import Notice from '../components/utils/notice/notice';
 import Script from 'next/script';
 
 export default function Home() {
@@ -28,19 +29,25 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.notice_container}>
+      <div className={styles.news_container}>
         <div className={styles.latest_notice}>
           <h2>最新のお知らせ</h2>
-          <div className={styles.notice_item}>お知らせ1</div>
-          <div className={styles.notice_item}>お知らせ2</div>
-          <div className={styles.notice_item}>お知らせ3</div>
+          <div className={styles.notice_container}>
+            <Notice href='https://kuresugutaira.com/'>
+              <h2>ホームページを一新しました</h2>
+              <p>変更前より情報が見やすくなりました</p>
+            </Notice>
+            <div className={styles.notice_item}>お知らせ1</div>
+            <div className={styles.notice_item}>お知らせ2</div>
+            <div className={styles.notice_item}>お知らせ3</div>
+          </div>
         </div>
         <div className={styles.twitter}>
           <h2>Twitter</h2>
           <a
             className='twitter-timeline'
             data-lang='ja'
-            data-height='1000'
+            data-height='500'
             data-theme='dark'
             href='https://twitter.com/Kuresugutaira?ref_src=twsrc%5Etfw'
           >
@@ -50,7 +57,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        <p>&copy; 2020 kuresugutaira</p>
+        <p>&copy; 2022 kuresugutaira</p>
       </footer>
     </>
   );
