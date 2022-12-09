e6a45d0650767ef6574aa4bec40f72e83c08f223
kuresugutaira <kuresugutaira@gmail.com>
2022-12-09 10:28:49 +0900

    index.tsxの微修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 import Link from 'next/link';
 import Icon from '../components/utils/icon/icon';
-import MainLayout from '../layouts/mainLayout';
 
 export default function Home() {
   return (
@@ -14,9 +12,9 @@ export default function Home() {
         <link rel='icon' href='/kuresugutaira_fav.ico' />
       </Head>
 
-      <Icon />
+      <h1>くれすぐたいら</h1>
 
-      <h1>くれすぐたいらのWebsite</h1>
+      <Icon />
 
       <p>
         情報系の学部に所属している大学3年生です。 UnityとC#を用いてゲームを制作したり、作曲したりしています。
