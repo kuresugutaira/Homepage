7b2029746fb4bc091dc1b2613e1fe49792777c61
kuresugutaira <kuresugutaira@gmail.com>
2022-12-11 13:42:35 +0900

    Iconコンポーネントを様々な場面使えるように修正

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,14 +1,22 @@
 import Image from 'next/image';
 import styles from './icon.module.scss';
 
-function Icon(): JSX.Element {
+// number | `&{number}` はImageのheight・widthが受け取るSafeNumber型と同じ型
+type IconProps = {
+  src: string;
+  size: number | `${number}` | undefined;
+  alt: string;
+  children?: never;
+};
+
+function Icon({ src, size, alt, children }: IconProps): JSX.Element {
   return (
     <Image
-      src='/images/kuresugutaira_icon.jpg'
-      height={200}
-      width={200}
-      alt='kuresugutairaのアイコン'
-      className={styles.roundIcon}
+      src={src}
+      height={size}
+      width={size}
+      alt={alt}
+      className={styles.icon}
     />
   );
 }
