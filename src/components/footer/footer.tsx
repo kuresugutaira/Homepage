1f271c0187a84776e938a23504804e1bf728802f
kuresugutaira <kuresugutaira@gmail.com>
2023-08-05 13:55:05 +0900

    copyrightの発行年を2022から2023に変更

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.scss';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2022 kuresugutaira</p>
+      <p>&copy; 2023 kuresugutaira</p>
     </footer>
   );
 }
