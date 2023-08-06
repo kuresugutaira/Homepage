71e97de7957efd2d2ae558fd27044a05f9756b7e
kuresugutaira <kuresugutaira@gmail.com>
2023-08-06 20:23:26 +0900

    NoticeページのデータをDBから持ってくる形にした

--- src/pages/notice.tsx
+++ src/pages/notice.tsx
@@ -1,40 +1,45 @@
+import { GetStaticProps } from 'next';
 import styles from '../styles/notice.module.scss';
 import NoticeCard from 'src/components/noticeCard/noticeCard';
+import type { Notice } from 'src/types/notice';
+import { client } from 'src/libs/client';
+import { SimplifyDate } from 'src/utils/util';
 
-function Notice() {
+type Props = {
+  notice: Notice[];
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  // Noticeのデータを全部取得する
+  const data = await client.get({
+    endpoint: 'notice',
+  });
+
+  return {
+    props: {
+      notice: data.contents,
+    },
+  };
+};
+
+function Notice({ notice }: Props) {
+  // notice_list: NoticeCardコンポーネントのリスト
+  // ビルド時にgetStaticPropsで持ってきたデータ(notice)を
+  // NoticeCardコンポーネントに変換してnotice_listに入れる
+  const notice_list: JSX.Element[] = [];
+  for (const e of notice) {
+    notice_list.push(
+      <NoticeCard
+        href={e.link}
+        title={e.title}
+        description={e.description}
+        date={SimplifyDate(e.date)}
+      />,
+    );
+  }
   return (
     <>
-      <div className={styles.notice_card_grid}>
-        <NoticeCard
-          href='https://soundcloud.com/vyy42cvqfubx/bright-highway'
-          title='新曲 Bright Highway を公開しました'
-          description='DnB(仮)です'
-          date='2022/1/3'
-        />
-        <NoticeCard
-          href='https://soundcloud.com/vyy42cvqfubx/iigjgjz6smtm'
-          title='新曲 夏と宇宙 を公開しました'
-          description='夏と宇宙をイメージしました'
-          date='2021/12/3'
-        />
-        <NoticeCard
-          href='https://soundcloud.com/vyy42cvqfubx/childlike'
-          title='新曲 Childlike を公開しました'
-          description='kawaii future bassです'
-          date='2021/3/25'
-        />
-        <NoticeCard
-          href='https://unityroom.com/games/takarabako-atsume'
-          title='新作ゲーム 宝箱あつめ を公開しました'
-          description='宝箱を集めていくゲームです'
-          date='2021/02/03'
-        />
-        <NoticeCard
-          href='https://kuresugutaira.com/'
-          title='ホームページを一新しました'
-          description='変更前より情報が見やすくなりました'
-        />
-      </div>
+      <div className={styles.notice_card_grid}>{notice_list}</div>
     </>
   );
 }
