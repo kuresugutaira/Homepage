c9695acd22ab09fbaa4ddb40f5d5f35f1a5af73d
kuresugutaira <kuresugutaira@gmail.com>
2023-08-06 20:13:53 +0900

    worksのページのデータをDBから取ってくるように変更、util.tsに関数を追加、util.tsxに変更

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,74 +1,42 @@
+import { Work } from 'src/types/work';
 import styles from '../styles/works.module.scss';
 import WorkCard from 'src/components/workCard/workCard';
+import { GetStaticProps } from 'next';
+import { client } from 'src/libs/client';
+import { BackSlashNToBR, ImageFileNameToURL } from 'src/utils/util';
 
-function Works() {
-  return (
-    <div className={styles.work_card_grid}>
+type Props = {
+  works: Work[];
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  // Aboutのデータを全部取得する
+  const data = await client.get({
+    endpoint: 'works',
+  });
+
+  return {
+    props: {
+      works: data.contents,
+    },
+  };
+};
+
+function Works({ works }: Props) {
+  const work_list: JSX.Element[] = [];
+  for (const e of works) {
+    work_list.push(
       <WorkCard
-        href='https://soundcloud.com/vyy42cvqfubx/rhythmi'
-        imageURL='/images/ryithmi.png'
-        title='Ryithmi'
+        href={e.link}
+        imageURL={ImageFileNameToURL(e.imageFileName)}
+        title={e.title}
       >
-        リズミカルな曲を
-        <br />
-        意識して作りました
-      </WorkCard>
-      <WorkCard
-        href='https://drive.google.com/drive/folders/1M5BWo_felWiwZwMoeTdZZVsm4DeTk-tH?usp=sharing'
-        imageURL='/images/dungeon_run.png'
-        title='Dungeon Run'
-      >
-        サークルで制作した
-        <br />
-        3Dのランゲームです
-      </WorkCard>
-      <WorkCard
-        href='https://unityroom.com/games/kainaoshi_atutyu'
-        imageURL='/images/kai.png'
-        title='宇宙で回なおし'
-      >
-        unity1weekにて制作した
-        <br />
-        アクションゲームです
-      </WorkCard>
-      <WorkCard
-        href='https://unityroom.com/games/takarabako-atsume'
-        imageURL='/images/collect_treasure.png'
-        title='宝箱あつめ'
-      >
-        タイルを踏んだり宝箱を
-        <br />
-        集めたりするゲームです
-      </WorkCard>
-      <WorkCard
-        href='https://soundcloud.com/vyy42cvqfubx/childlike'
-        imageURL='/images/childlike.png'
-        title='Childlike'
-      >
-        kawaii future bassの
-        <br />
-        練習で制作した曲です
-      </WorkCard>
-      <WorkCard
-        href='https://soundcloud.com/vyy42cvqfubx/iigjgjz6smtm'
-        imageURL='/images/cosmosummer.png'
-        title='夏と宇宙'
-      >
-        夏と宇宙をイメージして
-        <br />
-        制作した曲です
-      </WorkCard>
-      <WorkCard
-        href='https://soundcloud.com/vyy42cvqfubx/bright-highway'
-        imageURL='/images/bright_highway.png'
-        title='Bright Highway'
-      >
-        DnBの練習で制作した
-        <br />
-        ちょっと明るい曲です
-      </WorkCard>
-    </div>
-  );
+        {BackSlashNToBR(e.description)}
+      </WorkCard>,
+    );
+  }
+
+  return <div className={styles.work_card_grid}>{work_list}</div>;
 }
 
 export default Works;
--- src/utils/util.tsx
+++ src/utils/util.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // numOfDigitsの桁数に合わせてvalをゼロパディングする
 // ZeroPadding(28, 4) -> "0028"
 export function ZeroPadding(val: number, numOfDigits: number): string {
@@ -15,4 +17,12 @@ export function SimplifyDate(date_str: string): string {
 // 画像のファイル名からその画像にアクセスするためのパスを返す
 export function ImageFileNameToURL(imageFileName: string): string {
   return `/images/${imageFileName}`;
+}
+
+// 文字列'\n'がある部分をJSXが認識できる形の<br>タグにする
+export function BackSlashNToBR(str: string): JSX.Element {
+  const texts = str
+    .split(/(\n)/)
+    .map((item, index) => <React.Fragment key={index}>{item.match(/\n/) ? <br /> : item}</React.Fragment>);
+  return <div> {texts} </div>;
 }
