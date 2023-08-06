bbbaaa2be579a25b8d57738597b1414cacbeba9d
kuresugutaira <kuresugutaira@gmail.com>
2023-08-06 19:34:46 +0900

    データに関するtypeを追加

---
+++ src/types/about.ts
@@ -0,0 +1,12 @@
+export type About = {
+  id: string;
+  title: string;
+  acountName: string;
+  iconImageFileName: string;
+  link: string;
+  isRoundIcon: boolean;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
